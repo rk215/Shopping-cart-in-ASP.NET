e84e86693c7714524afbf651e3737438fc16d22e
Rk <r.kprajapati215@gmail.com>
2017-04-27 11:35:52 +0530

    File for uploading product details
    This File is used to upload product.

---
+++ Add Dummy Product.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Add Dummy Product.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="TextBox1" placeholder="name" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox2" placeholder="description" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox3" placeholder="price" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server"            Text="Button" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Add Dummy Product.aspx.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+Partial Class _Default
+    Inherits System.Web.UI.Page
+    Dim con As New SqlConnection(ConfigurationManager.ConnectionStrings("testConnectionString").ConnectionString)
+    Dim cmd As SqlCommand
+    Dim ad As SqlDataAdapter
+    Dim ds As New DataSet
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'FOR INSERTING ALL INFORMATION TO PRODUCT TABLE.
+
+        Dim path As String
+        path = Server.MapPath("Images/")
+        path = path + FileUpload1.FileName
+        FileUpload1.SaveAs(path)
+        path = "Images/" + FileUpload1.PostedFile.FileName
+
+        'STORE IMAGE IN FOLLOWING FORMAT. "FOLDERNAME/IMAGENAME.EXTENTsION.
+
+        cmd = New SqlCommand("insert into products values(@nm,@desc,@price,@img)", con)
+        cmd.Parameters.AddWithValue("@img", path)
+        cmd.Parameters.AddWithValue("@nm", TextBox1.Text)
+        cmd.Parameters.AddWithValue("@desc", TextBox2.Text)
+        cmd.Parameters.AddWithValue("@price", TextBox3.Text)
+        con.Open()
+        cmd.ExecuteNonQuery()
+        con.Close()
+        Response.Write("added")
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+    End Sub
+End Class
